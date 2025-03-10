2b212f0c50343aa0b70503e4b6cf82d43eea1ea6
martaverfer <martaverfer8@gmail.com>
2025-03-10 14:41:09 +0100

    lab extra for sql intro updated

--- challengeTask.sql
+++ challengeTask.sql
@@ -6,18 +6,18 @@ ORDER BY top_5_complaint DESC
 LIMIT 5;
 
 -- Analyze Complaints by Borough
-SELECT borough, COUNT(DISTINCT complaint_type) AS complaints_by_borough
+SELECT borough, COUNT(*) AS complaints_by_borough
 FROM nyc_311.nyc_311_service_requests
 GROUP BY borough;
 
 -- Filtering High-Volume Complaint Types
-SELECT complaint_type, COUNT(DISTINCT complaint_type) AS complaint_count
+SELECT complaint_type, COUNT(*) AS complaint_count
 FROM nyc_311.nyc_311_service_requests
 GROUP BY complaint_type
 HAVING complaint_count > 1000;
 
 -- Comparison of Boroughs for a Specific Complaint Type
-SELECT borough, COUNT(DISTINCT complaint_type) AS complaint_count
+SELECT borough, COUNT(*) AS complaint_count
 FROM nyc_311.nyc_311_service_requests
 WHERE complaint_type = 'Illegal Parking'
 GROUP BY borough;
